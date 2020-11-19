83e26c60476b025ab2eec39628cd309bd1a5419a
hb15331 <hb15331@outlook.com>
2020-11-19 17:39:03 -0500

    connect to Angular client

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+               'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+               'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+
+
 // share the app with quizService
 const quizController = require("./controllers/quizzes-controller")
 quizController(app)
